1aab090c60efdfa97b2fe9041d6b7b31db118a21
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-08 21:55:58 +0530

    Day 29 Practice Problem IndianCensus Program is pushed

---
+++ IndianCensusAnalyzer/CesusData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStateCesusAnalyzer
+{
+    public class CesusData
+    {
+        public string State { get; set; }
+        public string Population { get; set; }
+        public string Density { get; set; }
+        public string Literacy { get; set; }
+
+    }
+}
---
+++ IndianCensusAnalyzer/ImportCSVData.cs
@@ -0,0 +1,62 @@
+﻿using CsvHelper;
+using IndianStateCesusAnalyzer;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace IndianCensusAnalyzer
+{
+    class ImportCSVData
+    {
+        string importPath;
+        public ImportCSVData(string path)
+        {
+            this.importPath = path;
+        }
+
+        public int ImportData()
+        {
+            int count = 0;
+            if (!File.Exists(importPath))
+            {
+                throw new StateCensusFileExcption("wrong File or File not exist");
+            }
+            if (Path.GetExtension(importPath) != ".csv")
+            {
+                throw new StateCensusTypeExcption("wrong type");
+            }
+
+            using (var reader = new StreamReader(importPath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                try
+                {
+                    var records = csv.GetRecords<CesusData>().ToList();
+
+
+                    Console.WriteLine("read data succesfully done");
+                    foreach (CesusData add in records)
+                    {
+                        count++;
+                        Console.Write(" " + add.State);
+                        Console.Write(" " + add.Population);
+                        Console.Write(" " + add.Density);
+                        Console.Write(" " + add.Literacy);
+                        Console.WriteLine("\n");
+                    }
+                }
+                catch (CsvHelper.HeaderValidationException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            return count;
+        }
+
+    }
+}
+
